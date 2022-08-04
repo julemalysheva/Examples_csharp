a89b10ac2a66512eecf6b436412ad0810af9f938
julemalysheva <julemalysheva@gmail.com>
2022-08-05 00:23:58 +0500

    Add Example008_Dog_Friends

---
+++ Example008_Dog_Friends/Program.cs
@@ -0,0 +1,27 @@
+﻿int distance = 10000;
+int FirstFriendSpeed = 1;
+int SecondFriendSpeed = 2;
+int DogSpeed = 5;
+int friend = 2;
+int time = 0;
+int count = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (FirstFriendSpeed + DogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (SecondFriendSpeed + DogSpeed);
+        friend = 1;
+    }
+
+    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine($"Собака пробежит {count} раз");
+
