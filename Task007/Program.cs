4e9f6e21391d5fcab8752673971c56316aabd135
julemalysheva <julemalysheva@gmail.com>
2022-08-06 21:45:27 +0500

    Add Task007

---
+++ Task007/Program.cs
@@ -0,0 +1,14 @@
+﻿// 7. Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+// 	456 -> 6	782 -> 2	918 -> 8
+
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int a = number % 10;
+Console.WriteLine(a);
+
+//Дальше тренируюсь с типом String
+
+Console.Write("Введите в строку трехзначное число: ");
+String Str = Console.ReadLine();
+Console.WriteLine(Str.Substring(2));
