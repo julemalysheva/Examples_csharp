ea9b0db8164ead0bc688b6b8a640af344e614f41
julemalysheva <julemalysheva@gmail.com>
2022-08-07 21:05:30 +0500

    Ура! Описала универсальный метод поиска макс.в массиве

--- Example009_MyTest_IntroArray/Program.cs
+++ Example009_MyTest_IntroArray/Program.cs
@@ -1,14 +1,34 @@
-﻿//поиск макс. в массиве, как составить функцию???
- 
-int[] array = { 15, 72, 93, 484, 65, 16, 67, 887, 1999 };
-
-int result = array[0];
-int i = 0;
-
-while (i < array.Length)
+﻿//поиск макс. в массиве
+int MaxArray(int[] Array) //метод поиска и возврата макс.в массиве
 {
-        if (array[i] > result) result = array[i];
+    int i = 0;
+    int Max = Array[i];
+
+    while (i < Array.Length)
+    {
+        if (Array[i] > Max) Max = Array[i];
         i++;
+    }
+    return Max;
 }
 
-Console.WriteLine(result);
+int[] array = { 15, 72, 14593, 484, 65, 16, 67, 887 };
+
+int res = MaxArray(array);
+Console.WriteLine($"Макс.найден при помощи функции, равен {res}");
+
+
+//int result = array[0];
+//int i = 0;
+
+//while (i < array.Length)
+//{
+//        if (array[i] > result) result = array[i];
+//        i++;
+//}
+
+//Console.WriteLine(result);
+
+
+//Console.WriteLine();
+
