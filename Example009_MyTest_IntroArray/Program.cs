7dc0e3c7af443a1f0e0c586fd1b76ad1deb45067
julemalysheva <julemalysheva@gmail.com>
2022-08-07 19:13:35 +0500

    Example009_MyTest_IntroArray поиск макс в массиве в цикле

---
+++ Example009_MyTest_IntroArray/Program.cs
@@ -0,0 +1,14 @@
+﻿//поиск макс. в массиве, как составить функцию???
+ 
+int[] array = { 15, 72, 93, 484, 65, 16, 67, 887, 1999 };
+
+int result = array[0];
+int i = 0;
+
+while (i < array.Length)
+{
+        if (array[i] > result) result = array[i];
+        i++;
+}
+
+Console.WriteLine(result);
