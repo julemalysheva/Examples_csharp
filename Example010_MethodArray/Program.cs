1421edebdc7629a165622afb326c37770dc88055
julemalysheva <julemalysheva@gmail.com>
2022-08-07 19:35:04 +0500

    Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int [] array={71,29,34,4,45,66,4,85};
+int index=0;
+int find=4;
+
+while (index<array.Length)
+{
+    if (array[index]==find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
