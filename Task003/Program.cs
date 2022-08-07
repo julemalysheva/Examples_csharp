82f147b39bb57e4083c93a9fa919819ccf253394
julemalysheva <julemalysheva@gmail.com>
2022-08-07 16:51:39 +0500

    Task003 switch case

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,31 +1,25 @@
 ﻿// Задача 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
 Console.Write("Введите номер дня недели от 1 до 7: ");
 int number = int.Parse(Console.ReadLine());
-if (number == 1)
+
+switch (number)
 {
-    Console.WriteLine("Понедельник");
-}
-if (number == 2)
-{
-    Console.WriteLine("Вторник");
-}
-if (number == 3)
-{
-    Console.WriteLine("Среда");
-}
-if (number == 4)
-{
-    Console.WriteLine("Четверг");
-}
-if (number == 5)
-{
-    Console.WriteLine("Пятница");
-}
-if (number == 6)
-{
-    Console.WriteLine("Суббота");
-}
-if (number == 7)
-{
-    Console.WriteLine("Воскресенье");
+    case 1: Console.WriteLine("Понедельник");
+    break;
+    case 2: Console.WriteLine("Вторник");
+    break;
+    case 3: Console.WriteLine("Среда");
+    break;
+    case 4: Console.WriteLine("Четверг");
+    break;
+    case 5: Console.WriteLine("Пятница");
+    break;
+    case 6: Console.WriteLine("Суббота");
+    break;
+    case 7: Console.WriteLine("Воскресенье");
+    break;
+    default:
+        Console.WriteLine("Введённое значение вне диапазона 1..7");
+        break;
 }
