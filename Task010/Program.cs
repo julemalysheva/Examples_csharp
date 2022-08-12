0e75e584f0e13ce4842223025fcb740ade46995d
julemalysheva <julemalysheva@gmail.com>
2022-08-12 17:16:20 +0500

    change task010

--- Task010/Program.cs
+++ Task010/Program.cs
@@ -4,8 +4,20 @@
 782 -> 8
 918 -> 1
 */
-
+//второй вариант решения:
 Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num / 100 < 10 && num / 100 > 0) //проверяю, 3х-значное ли число
+{
+    int des = num % 100 / 10; //получаем 2ую цифру целочисленным делением двузначного числа, 
+    //полученного как остаток от деления на 100
+    Console.WriteLine(des);
+}
+else Console.WriteLine("Число не является трехзначным");
+
+
+/*Console.WriteLine("Введите трехзначное число: ");
 int num = Math.Abs(int.Parse(Console.ReadLine())); 
 int Ed = num % 10;//сколько единиц
 int Sotni = num / 100;//сколько сотен
@@ -15,4 +27,6 @@ else
 {
     int desCol = (num - Ed - Sotni * 100) / 10;
     Console.WriteLine(desCol);
-}
+}*/
+
+
