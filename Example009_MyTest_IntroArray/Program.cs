a61fd6f5884d0fdafe989d8d7ab4eda36aa04ffb
julemalysheva <julemalysheva@gmail.com>
2022-08-15 17:44:09 +0500

    Метод поиска макс.в массиве - цикл for

--- Example009_MyTest_IntroArray/Program.cs
+++ Example009_MyTest_IntroArray/Program.cs
@@ -1,18 +1,19 @@
 ﻿//поиск макс. в массиве
 int MaxArray(int[] Array) //метод поиска и возврата макс.в массиве
 {
-    int i = 0;
-    int Max = Array[i];
+    //int i = 0;
+    int Max = Array[0];
 
-    while (i < Array.Length)
+    //while (i < Array.Length) //заменила цикл на for
+    for (int i = 1; i < Array.Length; i++)
     {
         if (Array[i] > Max) Max = Array[i];
-        i++;
+        //i++;
     }
     return Max;
 }
 
-int[] array = { 15, 72, 14593, 484, 65, 16, 67, 887 };
+int[] array = { 100015, 5472, 593, 484, 8565, 16, 67, 887 };
 
 int res = MaxArray(array);
 Console.WriteLine($"Макс.найден при помощи функции, равен {res}");
