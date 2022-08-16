dc3f4494b0d0d6dec8f943eeb4692fad582cf25c
julemalysheva <julemalysheva@gmail.com>
2022-08-17 00:24:31 +0500

    add Task026

---
+++ Task026/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5*/
+
+int CountNumber(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        count++;
+        num /= 10;
+    }
+    return count;
+}
+
+Console.Write("Число = ");
+int number = int.Parse(Console.ReadLine());
+int CountN = CountNumber(number);
+Console.WriteLine(CountN);
