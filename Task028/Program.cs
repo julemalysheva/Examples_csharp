8ceb08ad4d5446318207b14b8c7b124583225a51
julemalysheva <julemalysheva@gmail.com>
2022-08-17 00:24:53 +0500

    add Task028

---
+++ Task028/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 -> 24 
+5 -> 120*/
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(GetF(num));
+
+int GetF(int num)
+{
+    int F = 1;
+    for(int i=2; i <= num; i++)
+    {
+        F *= i;
+    }
+    return F;
+}
