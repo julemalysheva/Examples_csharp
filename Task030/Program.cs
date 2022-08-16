a61ad8cb29cb5decd90e2846c809affecc3c62ed
julemalysheva <julemalysheva@gmail.com>
2022-08-17 00:25:10 +0500

    add Task030

---
+++ Task030/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задача 30: Напишите программу, которая выводит массив из 8 элементов, 
+заполненный нулями и единицами в случайном порядке.
+[1,0,1,1,0,1,0,0]*/
+
+
+void PrintArr()
+{
+    int[] arr = new int[8];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+        Console.Write(arr[i]);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+
+
+PrintArr();
