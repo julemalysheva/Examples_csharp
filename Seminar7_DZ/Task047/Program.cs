e4e7d4e04c3f9d7adacaa7af127b79b7cdf65700
julemalysheva <julemalysheva@gmail.com>
2022-08-29 09:30:03 +0500

    Task047

---
+++ Seminar7_DZ/Task047/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.*/
+
+double[,] GetArray(int row, int col, int minValue, int maxValue)
+{
+    double[,] arr = new double[row, col];
+    for (int i = 0; i < arr.GetLength(0); i++)
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Math.Round((new Random().Next(minValue, maxValue)
+                                   + new Random().NextDouble()), 2);
+        }
+    return arr;
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Число строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Число столбцов: ");
+int col = int.Parse(Console.ReadLine());
+Console.Write("Начало диапазона значений: ");
+int beginNum = int.Parse(Console.ReadLine());
+Console.Write("Окончание диапазона: ");
+int endNum = int.Parse(Console.ReadLine());
+
+double[,] myArray = GetArray(row, col, beginNum, endNum);
+PrintArray(myArray);
