120e4af755ed88587d676f4710ff7e57164f0ec3
julemalysheva <julemalysheva@gmail.com>
2022-08-31 18:24:00 +0500

    Seminar8_DZ\Task054

---
+++ Seminar8_DZ/Task054/Program.cs
@@ -0,0 +1,71 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы
+//каждой строки двумерного массива.
+
+int[,] GetArray(int row, int col, int beginNum, int endNum)
+{
+    int[,] arr = new int[row, col];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(beginNum, endNum + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void BubbleSort(int[] arr)
+{
+    for (int i = 1; i < arr.Length; i++)
+    {
+        for (int j = 0; j < arr.Length - i; j++)
+        {
+            if (arr[j] < arr[j + 1])
+            {
+                (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+            }
+        }
+    }
+}
+
+void DescendingSortOfRowArray(int[,] arr)
+{
+    int[] sortArr = new int[arr.GetLength(1)];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sortArr[j] = arr[i, j];
+        }
+        BubbleSort(sortArr);
+        for (int k = 0; k < sortArr.Length; k++)
+        {
+            arr[i, k] = sortArr[k];
+        }
+    }
+}
+
+Console.WriteLine("Задаем массив");
+Console.Write("Число строк: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Число столбцов: ");
+int col = int.Parse(Console.ReadLine());
+
+int[,] myArray = GetArray(row, col, 1, 10);
+PrintArray(myArray);
+Console.WriteLine();
+Console.WriteLine("Сортированный массив");
+DescendingSortOfRowArray(myArray);
+PrintArray(myArray);
