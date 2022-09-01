b4e4b6c489a9aa148ea0aefc51831c3e1f069254
julemalysheva <julemalysheva@gmail.com>
2022-09-01 18:26:25 +0500

    Seminar8_DZ\Task060

---
+++ Seminar8_DZ/Task060/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1) */
+
+int[,,] GetArrayNonRepeating(int row, int col, int thirdSize)
+{
+    int[,,] arr = new int[row, col, thirdSize];
+    Queue<int> SequenceForArray = new Queue<int>(Enumerable.Range(10, 90).OrderBy(_ => new Random().Next()));
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[i, j, k] = SequenceForArray.Dequeue();
+            }
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                Console.Write($"{arr[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.WriteLine("Задан массив");
+
+int[,,] myArray = GetArrayNonRepeating(2, 2, 2);
+PrintArray(myArray);
